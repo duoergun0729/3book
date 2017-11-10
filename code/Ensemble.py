46f6ff17707948ac869a8fd0acf7c37412c7e707
maidou <250261953@qq.com>
2017-11-10 11:51:27 +0800

    add

--- code/Ensemble.py
+++ code/Ensemble.py
@@ -9,18 +9,12 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from matplotlib import pyplot as plt
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import BaggingClassifier
 
 
-def func1():
-    x, y = datasets.make_classification(n_samples=1000, n_features=100,n_redundant=0, random_state = 1)
-    train_X, test_X, train_Y, test_Y = train_test_split(x,
-                                                        y,
-                                                        test_size=0.2,
-                                                        random_state=66)
-    knn = KNeighborsClassifier(n_neighbors=5)
-    knn.fit(train_X, train_Y)
-    pred_Y = knn.predict(test_X)
-
+def report(test_Y, pred_Y):
     print "accuracy_score:"
     print metrics.accuracy_score(test_Y, pred_Y)
     print "f1_score:"
@@ -36,7 +30,6 @@ def func1():
 
     f_pos, t_pos, thresh = metrics.roc_curve(test_Y, pred_Y)
     auc_area = metrics.auc(f_pos, t_pos)
-
     plt.plot(f_pos, t_pos, 'darkorange', lw=2, label='AUC = %.2f' % auc_area)
     plt.legend(loc='lower right')
     plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
@@ -45,5 +38,50 @@ def func1():
     plt.xlabel('False Pos Rate')
     plt.show()
 
+
+
+def func1():
+    x, y = datasets.make_classification(n_samples=1000, n_features=100,n_redundant=0, random_state = 1)
+    train_X, test_X, train_Y, test_Y = train_test_split(x,
+                                                        y,
+                                                        test_size=0.2,
+                                                        random_state=66)
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(train_X, train_Y)
+    pred_Y = knn.predict(test_X)
+
+def func2():
+    x, y = datasets.make_classification(n_samples=1000, n_features=100,n_redundant=0, random_state = 1)
+    train_X, test_X, train_Y, test_Y = train_test_split(x,
+                                                        y,
+                                                        test_size=0.2,
+                                                        random_state=66)
+    clf = AdaBoostClassifier(n_estimators=100)
+    clf.fit(train_X, train_Y)
+    pred_Y = clf.predict(test_X)
+    report(test_Y, pred_Y)
+
+def func3():
+    x, y = datasets.make_classification(n_samples=1000, n_features=100,n_redundant=0, random_state = 1)
+    train_X, test_X, train_Y, test_Y = train_test_split(x,
+                                                        y,
+                                                        test_size=0.2,
+                                                        random_state=66)
+    clf = GradientBoostingClassifier(n_estimators=100)
+    clf.fit(train_X, train_Y)
+    pred_Y = clf.predict(test_X)
+    report(test_Y, pred_Y)
+
+def func4():
+    x, y = datasets.make_classification(n_samples=1000, n_features=100,n_redundant=0, random_state = 1)
+    train_X, test_X, train_Y, test_Y = train_test_split(x,
+                                                        y,
+                                                        test_size=0.2,
+                                                        random_state=66)
+    clf = BaggingClassifier(n_estimators=100)
+    clf.fit(train_X, train_Y)
+    pred_Y = clf.predict(test_X)
+    report(test_Y, pred_Y)
+
 if __name__ == '__main__':
-    func1()
+    func4()
