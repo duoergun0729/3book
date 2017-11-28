3d182907e511910a4ff483947f713d27c3b4acde
maidou <250261953@qq.com>
2017-11-28 19:04:28 +0800

    add

---
+++ code/aisec.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+import os,sys,fnmatch
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import LSTM
+from keras.optimizers import RMSprop
+from keras.utils.data_utils import get_file
+import numpy as np
+import random
+
+
+
+html_dir="aisec/data/tomcat.apache.org/"
+model_file="aisec.h5"
+epochs=10
+
+
+
+def search_file(pattern="*.txt", root=os.curdir):
+    for path, dirs, files in os.walk(os.path.abspath(root)):
+        for filename in fnmatch.filter(files, pattern):
+            yield os.path.join(path, filename)
+
+def load_html_files(root_path):
+    file_suffix="*.js"
+    return search_file(file_suffix, root_path)
+
+def sample(preds, temperature=1.0):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+def generate(text, maxlen = 40,step = 40):
+    print('corpus length:', len(text))
+
+    chars = sorted(list(set(text)))
+    print('total chars:', len(chars))
+    char_indices = dict((c, i) for i, c in enumerate(chars))
+    indices_char = dict((i, c) for i, c in enumerate(chars))
+
+    # cut the text in semi-redundant sequences of maxlen characters
+
+    sentences = []
+    next_chars = []
+    for i in range(0, len(text) - maxlen, step):
+        sentences.append(text[i: i + maxlen])
+        next_chars.append(text[i + maxlen])
+    print('nb sequences:', len(sentences))
+
+    print('Vectorization...')
+    x = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+    y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+    for i, sentence in enumerate(sentences):
+        for t, char in enumerate(sentence):
+            x[i, t, char_indices[char]] = 1
+        y[i, char_indices[next_chars[i]]] = 1
+
+    # build the model: a single LSTM
+    print('Build model...')
+    model = Sequential()
+    model.add(LSTM(128, input_shape=(maxlen, len(chars))))
+    model.add(Dense(len(chars)))
+    model.add(Activation('softmax'))
+
+    optimizer = RMSprop(lr=0.01)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+    def sample(preds, temperature=1.0):
+        # helper function to sample an index from a probability array
+        preds = np.asarray(preds).astype('float64')
+        preds = np.log(preds) / temperature
+        exp_preds = np.exp(preds)
+        preds = exp_preds / np.sum(exp_preds)
+        probas = np.random.multinomial(1, preds, 1)
+        return np.argmax(probas)
+
+    if os.path.exists(model_file):
+        model.load_weights(model_file)
+    else:
+        model.fit(x, y,
+                  batch_size=128,
+                  epochs=epochs)
+
+        model.save_weights(model_file)
+
+    # train the model, output generated text after each iteration
+    for iteration in range(1, 60):
+        print()
+        print('-' * 50)
+        print('Iteration', iteration)
+
+
+        start_index = random.randint(0, len(text) - maxlen - 1)
+
+        for diversity in [0.2, 0.5, 1.0, 1.2]:
+            print()
+            print('----- diversity:', diversity)
+
+            generated = ''
+            #sentence = text[start_index: start_index + maxlen]
+            sentence="<script>alert"
+            generated += sentence
+            print('----- Generating with seed: "' + sentence + '"')
+            sys.stdout.write(generated)
+
+            for i in range(400):
+                x_pred = np.zeros((1, maxlen, len(chars)))
+                for t, char in enumerate(sentence):
+                    x_pred[0, t, char_indices[char]] = 1.
+
+                preds = model.predict(x_pred, verbose=0)[0]
+                next_index = sample(preds, diversity)
+                next_char = indices_char[next_index]
+
+                generated += next_char
+                sentence = sentence[1:] + next_char
+
+                sys.stdout.write(next_char)
+                sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    html_files=load_html_files(html_dir)
+    text=""
+    for i in html_files:
+        html=open(i).read().lower()
+        text+=html
+    generate(text,maxlen = 40,step = 40)
